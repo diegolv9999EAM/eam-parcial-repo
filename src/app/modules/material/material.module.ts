af0f4fbaf24abaeaf541537ecf46ad2fcc9c3798
diegolv9999EAM <2320122003@eam.edu.co>
2018-10-06 01:02:29 -0500

    proyecto configurado hasta video5

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule,MatButtonModule],
+  exports:[MatButtonModule],
   declarations: []
 })
 export class MaterialModule { }
