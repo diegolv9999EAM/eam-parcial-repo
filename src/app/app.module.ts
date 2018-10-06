8e664e96d6820bec35f25e42b9f56099a32745a8
diegolv9999EAM <2320122003@eam.edu.co>
2018-10-06 00:24:29 -0500

    proyecto configuracion video3

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+//modulos
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './modules/material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+//componentes
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
